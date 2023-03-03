32a4fb9daff5e574891a8f5b44631a5a414f6779
Arthur <arthur.lucas@edu.devinci.fr>
2023-03-03 11:25:48 +0100

    done

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const { PythonShell } = require('python-shell');
-
+const cors = require('cors')
+const bodyParser = require('body-parser');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let i = 0
+let color = [0,0,0]
+
+function jump(){
+  if(character.classList == "animate"){return}
+  character.classList.add("animate");
+  setTimeout(function(){
+      character.classList.remove("animate");
+  },300);
+} 
 
 app.use(express.static('public'));
+app.use(cors())
 
-app.get('/data', (req, res) => {
-  let options = {
-    mode: 'text',
-    pythonOptions: ['-u'], // get print results in real-time
-    scriptPath: './',
-    args: []
-  };
+app.get('/jump', (req, res) => {
+  console.log('jump');
 
-  PythonShell.run('script.py', options, (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
+  res.send('Données reçues !');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(3000, () => {
+  console.log('Serveur en écoute sur le port 3000...');
+});
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+
+
+
--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
+var MyApp = require('../app');
+// Code à exécuter une fois que la page est chargée
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
+
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
@@ -11,13 +14,13 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
+    // if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    //     block.style.animation = "none";
+    //     // alert("Game Over. score: "+Math.floor(counter/100));
+    //     counter=0;
+    //     block.style.animation = "block 1s infinite linear";
+    // }else{
+    //     counter++;
+    //     document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    // }
 }, 10);
